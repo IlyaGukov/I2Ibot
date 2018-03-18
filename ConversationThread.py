66e1d88d0c79b22984547fe19ba0e3f07c830d91
emil davletov <valgger@yandex.ru>
2018-03-18 23:12:03 +0300

    don't remember what was changed

--- ConversationThread.py
+++ ConversationThread.py
@@ -1,18 +1,12 @@
 from Oracle import Oracle
 
 class Conversation:
-	oracle = Oracle()
-	
-	def __init__(self, question, asker_id):
-		self.bot = bot
-		self.question = question
-		self.asker_id = asker_id
-		self.answer = {}
-		
-	def main(self):
-		oracle.push_to_queue(self.question)
-		
-
-	
-	
-	
+    oracle = Oracle()
+    
+    def __init__(self, question, asker_id):
+        self.question = question
+        self.asker_id = asker_id
+        self.answer = {}
+        
+    def main(self):
+        oracle.push_to_queue(self.question)
