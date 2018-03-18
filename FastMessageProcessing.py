9330eaaf0fef2da9b9423947178af91302073acb
emil davletov <valgger@yandex.ru>
2018-03-18 23:13:46 +0300

    askers and registrators as UserTable instances and small corrections

--- FastMessageProcessing.py
+++ FastMessageProcessing.py
@@ -1,13 +1,17 @@
 from telegram.ext import Updater, CommandHandler, ConversationHandler, Filters
 from telegram.ext.dispatcher import run_async
+from UserTable import UserTable
 import threading
+import logging
 import queue
 
-askers_lock = threading.Lock()
-registrators_lock = threading.Lock()
+# Enable logging
+logging.basicConfig(format='FMP(%(asctime)s - %(name)s - %(levelname)s - %(message)s):',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-askers = set()
-registrators = set()
+askers_table = UserTable()
+registrators_table = UserTable()
 
 messages_queue = queue.Queue()
 
@@ -17,27 +21,34 @@ def _help(bot, update):
         'Hi, I am the bot that help to fastly find answers to your questions\n\n'\
         'Here is the test version, dont be rigorous.\n'\
         'First of all type */register* to register.\n'\
-        'You can register as guest to ask questions or as local to answer them.\n'\
-        'If you are guest and want to ask question, send */ask* before sending question.\n'\
-        'Then you can send a question in one message and wait while smb will answer.\n'\
+        'You can add more languages/themes using */edit_profile* \n'\
+        'If you want to ask question, send */ask* before sending question.\n'\
+        'Then you can send a question in one message and hope that smb will answer.\n'\
         'type */help* to see this message.'
         , parse_mode = 'Markdown')
+
+@run_async
+def _send_error(message_chat_id_, error_message):
+    bot.send_message(chat_id=message_chat_id_, text = error_message, parse_mode = 'Markdown')
 
 # на question и регистрейшн асинк может и не нужен
 # @run_async
 def _question(bot, update):
-    with askers_lock:
-        askers.add(update.message.chat_id)
+    if registrators_table.user_in_table(update.message.chat_id):
+        _send_error(update.message.chat_id,
+            'please end registration/profile_data_correction process before asking questions')
+    else:
+        logger.info("User %d added to askers_table", update.message.chat_id)
+        askers_table.add_user(update.message.chat_id)
 
 # @run_async
 def _registration(bot, update):
-    with registrators_lock:
-        registrators.add(update.message.chat_id)
+    logger.info("User %d added to registrators_table", update.message.chat_id)
+    registrators_table.add_user(update.message.chat_id)
 
 # @run_async
 def _message_handler(bot, update):
     messages_queue.put((update.message.chat_id, update.message.text))
-
 
 def main():
     token = ''
@@ -48,6 +59,9 @@ def main():
     dp.add_handler(help_handler, group = 1)
 
     registration_handler = CommandHandler('register', _registration)
+    dp.add_handler(registration_handler, group = 1)
+
+    registration_handler = CommandHandler('edit_profile', _registration)
     dp.add_handler(registration_handler, group = 1)
 
     question_handler = CommandHandler('ask', _question)
