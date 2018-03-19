c6053d4f2ffaa5c80d66668820d0def6215c5b65
emil davletov <valgger@yandex.ru>
2018-03-19 23:36:02 +0300

    question class

---
+++ Question.py
@@ -0,0 +1,19 @@
+class Question:
+    
+    def __init__(self, asker_id_, text_)
+        self.asker_id = asker_id_
+        self.text = text_
+        self.language = _detect_message_language(self.text)
+        self.theme = _detect_message_theme(self.text)
+
+    def _detect_message_language(message):
+    lang = detect_langs(message)[0]
+    if (lang.prob > 0.99):
+        lang = lang.lang
+    else:
+        lang = 'en'
+    return languages.get(alpha_2 = lang).name
+
+    #ToDo: implement
+    def _detect_message_theme(message):
+        return 'IT'
