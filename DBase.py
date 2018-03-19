f7e3a8417d26d4f9a17472eed9b51032fb02afcf
emil davletov <valgger@yandex.ru>
2018-03-20 02:17:14 +0300

    now it remeber user_data_base after server restart

--- DBase.py
+++ DBase.py
@@ -1,13 +1,21 @@
 import queue
 import threading
+import pickle
 
 class UserDataBase:
     def __init__(self):
-        self.data = dict()
+        try:
+            with open('user_db', 'rb') as f:
+                self.data = pickle.load(f)
+        except (FileNotFoundError, EOFError, IOError) as e:
+            self.data = dict()
+
     
     #ToDo make it async
     def add_user(self, chat_id, user_name):
         self.data[chat_id] = {'Name':user_name, 'Occupied':False, 'Languages': set(), 'Themes': set()}
+        with open('user_db', 'wb') as f:
+            pickle.dump(self.data, f, pickle.HIGHEST_PROTOCOL)
 
     def get_user(self, chat_id):
         return self.data[chat_id]
@@ -17,12 +25,16 @@ class UserDataBase:
 
     def write_user_data(self, chat_id, data_field, user_data):
         self.data[chat_id][data_field].add(user_data)
+        with open('user_db', 'wb') as f:
+            pickle.dump(self.data, f, pickle.HIGHEST_PROTOCOL)
 
     def change_occupation(self, chat_id):
         self.data[chat_id]['Occupied'] = not self.data[chat_id]['Occupied']
             
     def delete_user(self, chat_id):
         self.data.pop(chat_id)
+        with open('user_db', 'wb') as f:
+            pickle.dump(self.data, f, pickle.HIGHEST_PROTOCOL)
     
     
 class One_to_one:
